6607a05718f6bb0cfcefedaa1c9acef4411b6cac
twisti <twisti@slne.dev>
2024-02-19 15:12:35 +0100

    feat: added gtag

---
+++ Writerside/cfg/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PG8BDPRMPF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PG8BDPRMPF');
+</script>
